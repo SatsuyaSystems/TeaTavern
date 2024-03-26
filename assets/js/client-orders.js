789a43a3b897e97ee6d49508fef51c4bdfbf2619
angryzero <sidney.marvin.fricke@gmail.com>
2024-03-26 16:42:37 +0100

    new modal, more layout matching

--- assets/js/client-orders.js
+++ assets/js/client-orders.js
@@ -7,3 +7,25 @@ function loadOrder(event) {
     $('#order-payed').val(orderData['payed']);
     $('#order-shipped').val(orderData['shipped']);
 }
+
+
+
+function copyText(event) {
+    let copyText = event.currentTarget;
+    copyText.classList.toggle('rainbow_text_animated');
+
+    let copyTextValue = copyText.getAttribute('title');
+
+    // Create a temporary input field
+    let tempInput = document.createElement('input');
+    tempInput.setAttribute('style', 'display: none;')
+    tempInput.value = copyTextValue;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    navigator.clipboard.writeText(tempInput.value);
+    tempInput.remove();
+
+    setTimeout(() => {
+        copyText.classList.toggle('rainbow_text_animated');
+    }, 1000);
+}
